b8caa96d70cc93c5412443e3b334f03db0924302
Bicet <bicets@gmail.com>
2021-04-02 21:51:33 +0200

    FixUp!: Doubletap to Sleep on lockscreen
    Coming from doze it was not worming

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
@@ -3220,7 +3220,7 @@ public class NotificationPanelViewController extends PanelViewController {
                     mDoubleTapGesture.onTouchEvent(event);
                 }
 
-                if (mIsLockscreenDoubleTapEnabled
+                if (mIsLockscreenDoubleTapEnabled && !mPulsing && !mDozing
                         && mBarState == StatusBarState.KEYGUARD) {
                     mLockscreenDoubleTapToSleep.onTouchEvent(event);
                 }
